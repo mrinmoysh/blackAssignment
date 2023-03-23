3ae0c00eb565c1308f7561e7480c19106783ea5b
mrinmoysh <43840715+mrinmoysh@users.noreply.github.com>
2023-03-23 11:42:41 +0530

    secret remove

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-// const uri = "mongodb+srv://tanmoy:b6sMGSRwjFkeXNW0@black.cbhyafz.mongodb.net/?retryWrites=true&w=majority"
 
-//password- b6sMGSRwjFkeXNW0
 const connectDB = (uri) => {
     // console.log("mrs")
     return mongoose.connect(uri, {
